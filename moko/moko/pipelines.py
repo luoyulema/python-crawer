30b86567ac7bf8866230a4dd2fda933de603c1a9
luoyulema <1191281121@qq.com>
2016-11-08 11:32:42 +0800

    moko insert mysql

--- moko/moko/pipelines.py
+++ moko/moko/pipelines.py
@@ -6,6 +6,9 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 from moko.items import MokoItem
+import MySQLdb
+import MySQLdb.cursors
+from twisted.enterprise import adbapi
 
 
 class MokoPipeline(object):
@@ -14,9 +17,34 @@ class MokoPipeline(object):
         self.file = open('test.txt', 'w')
 
     def process_item(self, item, spider):
-        text = item['img_url']+'\n'
+        text = item['img_url'] + '\n'
         self.file.writelines(text)
         return item
 
     def close_spider(self, spider):
         self.file.close()
+
+
+class MokoMysqlPipeline(object):
+
+    def __init__(self):
+        self.dbpool = adbapi.ConnectionPool(
+            dbapiName='MySQLdb',
+            host='127.0.0.1',
+            db='moko',
+            user='root',
+            passwd='mydream5',
+            cursorclass=MySQLdb.cursors.DictCursor,
+            charset='utf-8',
+            use_unicode=False,
+
+        )
+
+    def process_item(self, item, spider):
+        query = self.dbpool.runInteraction(self.__conditional_insert, item)
+        return item
+
+    def __conditional_insert(self, tx, item):
+        parms = item['img_url']
+        sql = "insert into moko valuse %s" % parms
+        tx.execute(sql)
